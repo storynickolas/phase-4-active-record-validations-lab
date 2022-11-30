1badf8d565fa1e3021d4267e972f713fc1d6e95d
Nick Story <storynickolasl@gmail.com>
2022-11-29 20:14:57 -0500

    commit tree 0481e44e5a375c8232c4b378d1923047eff0b04a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: {  maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :clickbait
+
+  def clickbait
+    if title
+      unless title.include? "Won't Believe" || "Secret" || "Top" || "Guess"
+        errors.add(:title, "Not sufficiently Clickbait-y")
+      end
+    end
+  end
 end
